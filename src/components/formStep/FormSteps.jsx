273c6a0786e88a8e297a0cfae3193ea562b8d1e0
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-20 19:36:18 +0430

    Complete form steps sidebar

--- src/components/formStep/FormSteps.jsx
+++ src/components/formStep/FormSteps.jsx
@@ -1,6 +1,24 @@
-const FormSteps = () => {
+import { formStepsData } from "../../data";
+
+const FormSteps = ({ formStep }) => {
   return (
-    <div>FormSteps</div>
-  )
-}
-export default FormSteps
+    <aside className="sidebar">
+      <ul className="sidebar__steps">
+        {formStepsData.map(({ step, desc }) => (
+          <li key={step} className="step">
+            <div
+              className={
+                formStep === step ? "step__num step__num--active" : "step__num"
+              }
+            >
+              {step}
+            </div>
+            <span className="step__title">step {step}</span>
+            <span className="step__desc">{desc}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+export default FormSteps;

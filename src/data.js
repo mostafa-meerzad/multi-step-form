42d0f20b5c60d8e973e07a79b90a5c35cf87de0b
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-11-02 15:37:38 +0430

    Separate form-steps header data in the data file, to make it better organized

--- src/data.js
+++ src/data.js
@@ -20,80 +20,102 @@ const stepOneData = [
   {
     label: "Name",
     placeholder: "e.g. Stephen King",
-    type:"text",
-    name:"name"
+    type: "text",
+    name: "name",
   },
   {
     label: "Email Address",
     placeholder: "e.g. stephenking@lorem.com",
-    type:"email",
-    name:"email",
+    type: "email",
+    name: "email",
   },
   {
     label: "Phone Number",
     placeholder: "e.g. +1 234 567 890",
-    type:"text",
-    name:"phone"
-  }
-]
+    type: "text",
+    name: "phone",
+  },
+];
 
 const stepTwoData = [
   {
-    img:arcade,
+    img: arcade,
     title: "Arcade",
-    price:{
-      monthly:9,
-      yearly:90,
-    }, 
-    freeMonths: "2 months free"
+    price: {
+      monthly: 9,
+      yearly: 90,
+    },
+    freeMonths: "2 months free",
   },
   {
-    img:advanced,
+    img: advanced,
     title: "Advanced",
-    price:{
-      monthly:12,
-      yearly:120,
-    }, 
-    freeMonths: "2 months free"
+    price: {
+      monthly: 12,
+      yearly: 120,
+    },
+    freeMonths: "2 months free",
   },
   {
-    img:pro,
+    img: pro,
     title: "Pro",
-    price:{
-      monthly:15,
-      yearly:150,
-    }, 
-    freeMonths: "2 months free"
+    price: {
+      monthly: 15,
+      yearly: 150,
+    },
+    freeMonths: "2 months free",
   },
-]
+];
 
 const stepThreeData = [
-
   {
-    title:"Online service",
+    title: "Online service",
     desc: "Access to multiplayer games",
-    price:{
+    price: {
       monthly: 1,
-      yearly:10,
-    }
+      yearly: 10,
+    },
   },
   {
-    title:"Larger storage",
+    title: "Larger storage",
     desc: "Extra 1TB of cloud save",
-    price:{
+    price: {
       monthly: 2,
-      yearly:20,
-    }
+      yearly: 20,
+    },
   },
   {
-    title:"Customizable Profile",
+    title: "Customizable Profile",
     desc: "Custom theme on your profile",
-    price:{
+    price: {
       monthly: 2,
-      yearly:20,
-    }
+      yearly: 20,
+    },
   },
+];
 
-]
-
-export {formStepsData, stepOneData, stepTwoData, stepThreeData}
+const formStepsHeader = [
+  {
+    title: "Personal info",
+    desc: "Please provide your name, email address, and phone number.",
+  },
+  {
+    title: "Select your plan",
+    desc: "You have the option of monthly or yearly billing.",
+  },
+  {
+    title: "  Pick add-ons",
+    desc: "Add-ons help enhance your gaming experience.",
+  },
+  {
+    title: "  Finishing up",
+    desc: "  Double-check everything looks OK before confirming.",
+  },
+];
+export {
+  formStepsData,
+  stepOneData,
+  stepTwoData,
+  stepThreeData,
+  formStepsHeader,
+};
